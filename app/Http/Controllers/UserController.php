273be50b338f49e0b925df98f3822a7775bc7ad2
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-03 13:21:49 +0600

    3_user reg controller & pman_b

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use App\Models\User;
 
 class UserController extends Controller
 {
      public function UserRegistration(Request $request){
-         return User::create([
+       /*User::create([
             'firstName'=> $request->input('firstName'),
             'lastName' => $request->input('lastName'),
             'email' => $request->input('email'),
@@ -17,5 +18,32 @@ class UserController extends Controller
 
 
          ]);
+         return response()->json([
+            'status' => 'success',
+            'message' => 'User Registration Successfully'
+         ], 200);*/
+         try {
+            User::create([
+                'firstName' => $request->input('firstName'),
+                'lastName' => $request->input('lastName'),
+                'email' => $request->input('email'),
+                'mobile' => $request->input('mobile'),
+                'password' => $request->input('password'),
+            ]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Registration Successfully'
+            ],200);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'User Registration Failed'
+            ],200);
+
+        }
+
+
      }
+
 }
